10844e794d8507f8c27fdfa5e6b6c70fce4af0a9
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2025-07-06 12:08:29 +0530

    push the latest

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC_xDWrzsLMfannQqoNXVEPBg0S8u89Avw",
-  authDomain: "projectn-daee6.firebaseapp.com",
-  projectId: "projectn-daee6",
-  storageBucket: "projectn-daee6.firebasestorage.app",
-  messagingSenderId: "606662393376",
-  appId: "1:606662393376:web:ea8fd0da382c63371e8645",
-  measurementId: "G-WN7KDR7N0F",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
